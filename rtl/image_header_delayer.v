4ead9dd260fc27f89e4eac90eacca2df68b80645
Lane Brooks <dirjud@gmail.com>
2015-04-05 11:52:42 -0600

    updated image_header_delayer to include breaks appropriately

--- rtl/image_header_delayer.v
+++ rtl/image_header_delayer.v
@@ -25,16 +25,35 @@ module image_header_delayer
    output reg [DATA_WIDTH-1:0] 	 datao
    );
 
-   reg [5:0] 			 waddr, raddr;
-   reg 				 stalled;
-   parameter FIFO_SIZE = `Image_image_data + 5;
+   reg [6:0] 			 waddr, raddr;
+   reg 				 stalled1;
+
+   // stall1 is used to delay the image header until the next frame start is
+   // received. Then we release the image header. When the image header is
+   // done, we then enter stall2 to give some time down time for anything
+   // that may need some clocks cycles to close out the frame before sending
+   // the next start of frame signal.
+   
+   parameter NUM_STALL2_COUNTS = 40;
+   parameter FIFO_SIZE = `Image_image_data + 20 + NUM_STALL2_COUNTS;
+
    reg [`DTYPE_WIDTH + DATA_WIDTH - 1 : 0] fifo [0:FIFO_SIZE-1];
+
+   reg  [5:0] stall2_count;
+   wire [5:0] next_stall2_count = stall2_count - 1;
+   wire       stalled2 = |stall2_count;
+
+   wire [`DTYPE_WIDTH-1:0] dtype0;
+   wire [DATA_WIDTH-1:0]   data0;
+
+   assign { dtype0, data0 } = fifo[raddr];
    
    always @(posedge clk) begin
       if(!resetb) begin
 	 waddr   <= 0;
 	 raddr   <= 0;
-	 stalled <= 0;
+	 stalled1<= 0;
+	 stall2_count <= 0;
 	 datao   <= 0;
 	 dvo     <= 0;
 	 dtypeo  <= 0;
@@ -43,7 +62,8 @@ module image_header_delayer
 	    dvo     <= dvi;
 	    dtypeo  <= dtypei;
 	    datao   <= datai;
-	    stalled <= 0;
+	    stalled1 <= 0;
+	    stall2_count <= 0;
 
 	 end else begin
 	    if (dvi) begin
@@ -51,23 +71,38 @@ module image_header_delayer
 	       waddr <= (waddr == FIFO_SIZE-1) ? 0 : waddr + 1;
 	    end
 	    
-	    if (!stalled) begin
+	    if (!stalled1 && !stalled2) begin
 	       if (raddr != waddr) begin
 		  dvo <= 1;
-		  { dtypeo, datao } <= fifo[raddr];
+		  datao <= data0;
+		  dtypeo <= dtype0;
+		  if(dtype0 == `DTYPE_HEADER_END) begin
+		     stall2_count <= NUM_STALL2_COUNTS;
+		  end else if(dtype0 == `DTYPE_ROW_END) begin
+		     stall2_count <= 4;
+		  end else if(dtype0 == `DTYPE_FRAME_START) begin
+		     stall2_count <= 1;
+		  end
 		  raddr <= (raddr == FIFO_SIZE-1) ? 0 : raddr + 1;
 	       end else begin
 		  dvo <= 0;
 		  dtypeo <= 0;
 		  datao <= 0;
 	       end
+	    end else begin
+	       dvo <= 0;
+	       dtypeo <= 0;
+	       datao <= 0;
+	       if(stalled2) begin
+		  stall2_count <= next_stall2_count;
+	       end
 	    end
 	    
 	    // maintain header address for correct image header substitution
 	    if(dvi && dtypei == `DTYPE_HEADER_START) begin
-	       stalled <= 1;
+	       stalled1 <= 1;
 	    end else if(dvi && dtypei == `DTYPE_FRAME_START) begin
-	       stalled <= 0;
+	       stalled1 <= 0;
 	    end
 	 end
       end
