43f2d18db2e60393a43da254b2fcae27ec9f11af
Lane Brooks <dirjud@gmail.com>
2015-11-30 09:59:33 -0700

    updates to vignette

--- rtl/vignette.v
+++ rtl/vignette.v
@@ -14,7 +14,8 @@ module vignette
     parameter NUM_ROWS=728,
     parameter NUM_COLS=1286,
     parameter GAIN_WIDTH=8,
-    parameter GAIN_FRAC_WIDTH=6)
+    parameter GAIN_FRAC_WIDTH=6,
+    parameter SUBSAMPLE_SHIFT=0) // 2^SUB_SAMPLE_SHIFT cols/rows gain coeff get interpolated
    (
     input 			  clk,
     input 			  resetb,
@@ -46,8 +47,11 @@ module vignette
     output reg [15:0] 		  datao
     );
 
-   reg [GAIN_WIDTH-1:0] 	   col_coeff[0:NUM_COLS-1];
-   reg [GAIN_WIDTH-1:0] 	   row_coeff[0:NUM_ROWS-1];
+   parameter SUBSAMPLE = 1 << SUBSAMPLE_SHIFT;
+   parameter SUB_NUM_COLS = ((NUM_COLS+SUBSAMPLE-1)/SUBSAMPLE);
+   parameter SUB_NUM_ROWS = ((NUM_ROWS+SUBSAMPLE-1)/SUBSAMPLE);
+   reg [GAIN_WIDTH-1:0] col_coeff[0:SUB_NUM_COLS-1];
+   reg [GAIN_WIDTH-1:0] row_coeff[0:SUB_NUM_ROWS-1];
 
    wire di_col_en = di_term_addr == `TERM_VignetteCol;
    wire di_row_en = di_term_addr == `TERM_VignetteRow;
@@ -55,22 +59,13 @@ module vignette
    assign di_transfer_status = 0;
    assign di_read_rdy = 1;
    assign di_write_rdy = 1;
-   //assign di_reg_datao = 0; // Use this to disable read and reduce a read port to RAMs.
+   assign di_reg_datao = 0; // Use this to disable read and reduce a read port to RAMs.
    /* verilator lint_off WIDTH */
-   assign di_reg_datao = (di_col_en) ? col_coeff[di_reg_addr[DIM_WIDTH-1:0]] : row_coeff[di_reg_addr[DIM_WIDTH-1:0]];
+   //assign di_reg_datao = (di_col_en) ? col_coeff[di_reg_addr[DIM_WIDTH-1:0]] : row_coeff[di_reg_addr[DIM_WIDTH-1:0]];
    /* verilator lint_on WIDTH */
    integer idx;
-   always @(posedge di_clk or negedge resetb) begin
-      if(!resetb) begin
-	 /* verilator lint_off WIDTH */
-	 for(idx=0; idx<NUM_COLS; idx=idx+1) begin
-	    col_coeff[idx] = { 1'b1, { GAIN_FRAC_WIDTH { 1'b0 }}};
-	 end
-	 for(idx=0; idx<NUM_ROWS; idx=idx+1) begin
-	    row_coeff[idx] = { 1'b1, { GAIN_FRAC_WIDTH { 1'b0 }}};
-	 end
-	 /* verilator lint_on WIDTH */
-      end else if(di_en) begin
+   always @(posedge di_clk) begin
+      if(di_en) begin
 	 if(di_write) begin
 	    if(di_col_en) begin
 	       col_coeff[di_reg_addr] <= di_reg_datai[GAIN_WIDTH-1:0];
@@ -86,8 +81,35 @@ module vignette
    wire 			   valid_pixel = |(dtypei & `DTYPE_PIXEL_MASK);
 
    /* verilator lint_off WIDTH */
-   wire [GAIN_WIDTH-1:0] 	   col_coeff_sel = col_coeff[col_pos];
-   wire [GAIN_WIDTH-1:0] 	   row_coeff_sel = row_coeff[row_pos];
+
+   // interpolate the column gain
+   wire [DIM_WIDTH-SUBSAMPLE_SHIFT-1:0] cx0 = col_pos[DIM_WIDTH-1:SUBSAMPLE_SHIFT];
+   wire [DIM_WIDTH-SUBSAMPLE_SHIFT-1:0] cx1a = cx0+1;
+   wire [DIM_WIDTH-SUBSAMPLE_SHIFT-1:0] cx1 = (cx1a >= SUB_NUM_COLS) ? cx0:cx1a;
+   wire [GAIN_WIDTH-1:0] cy0 = col_coeff[cx0];
+   wire [GAIN_WIDTH-1:0] cy1 = col_coeff[cx1];
+   wire [SUBSAMPLE_SHIFT-1:0] cdx = col_pos[SUBSAMPLE_SHIFT-1:0];
+   wire [GAIN_WIDTH:0] dcy = (cy1 > cy0) ? cy1 - cy0 : cy0 - cy1;
+   wire [GAIN_WIDTH+SUBSAMPLE_SHIFT-1:0] cm = dcy * cdx;
+   wire [GAIN_WIDTH-1:0] 	      cm1 = cm >> SUBSAMPLE_SHIFT;
+   wire [GAIN_WIDTH-1:0] 	      cm2 = (cy1 > cy0) ? cy0 + cm1 : cy0 - cm1;
+   wire [GAIN_WIDTH-1:0] 	      col_coeff_sel = cm2;
+
+   // interpolate the row gain
+   wire [DIM_WIDTH-SUBSAMPLE_SHIFT-1:0] rx0 = row_pos[DIM_WIDTH-1:SUBSAMPLE_SHIFT];
+   wire [DIM_WIDTH-SUBSAMPLE_SHIFT-1:0] rx1a = rx0+1;
+   wire [DIM_WIDTH-SUBSAMPLE_SHIFT-1:0] rx1 = (rx1a >= SUB_NUM_ROWS) ? rx0:rx1a;
+   
+   wire [GAIN_WIDTH-1:0] ry0 = row_coeff[rx0];
+   wire [GAIN_WIDTH-1:0] ry1 = row_coeff[rx1];
+   wire [SUBSAMPLE_SHIFT-1:0] rdx = row_pos[SUBSAMPLE_SHIFT-1:0];
+   wire [GAIN_WIDTH:0] dry = (ry1 > ry0) ? ry1 - ry0 : ry0 - ry1;
+   wire [GAIN_WIDTH+SUBSAMPLE_SHIFT-1:0] rm = dry * rdx;
+   wire [GAIN_WIDTH-1:0] 	      rm1 = rm >> SUBSAMPLE_SHIFT;
+   wire [GAIN_WIDTH-1:0] 	      rm2 = (ry1 > ry0) ? ry0 + rm1 : ry0 - rm1;
+   wire [GAIN_WIDTH-1:0] 	      row_coeff_sel = rm2;
+   //wire [GAIN_WIDTH-1:0] 	      row_coeff_sel = row_coeff[row_pos[DIM_WIDTH-1:SUBSAMPLE_SHIFT]];
+   
    /* verilator lint_on WIDTH */
    
    wire [GAIN_WIDTH+PIXEL_WIDTH-1:0] mult0 = col_coeff_sel * datai[PIXEL_WIDTH-1:0];
